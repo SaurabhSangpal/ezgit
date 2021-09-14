1b1119ec16aab7aa279451924d5ee85b23067499
Saurabh Sangpal <saurabhsangpal@gmail.com>
2021-09-14 20:14:01 +0530

    Fix bug in return value of repo operations

--- src/git_wrapper/repository.cc
+++ src/git_wrapper/repository.cc
@@ -4,15 +4,15 @@
 
 bool git::Repository::Create(const char* path) noexcept {
 	int error = git_repository_init(&repo, path, 0);
-	return error != 0;
+	return error == 0;
 }
 
 bool git::Repository::Open(const char* path) noexcept {
 	int error = git_repository_open(&repo, path);
-	return error != 0;
+	return error == 0;
 }
 
 bool git::Repository::Clone(const char* url, const char* directory) noexcept {
 	int error = git_clone(&repo, url, directory, NULL);
-	return error != 0;
+	return error == 0;
 }
