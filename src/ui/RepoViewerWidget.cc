d8d4833df51c3ba90bb29e6ef7a18ac0b1892632
Saurabh Sangpal <saurabhsangpal@gmail.com>
2021-09-19 10:41:01 +0530

    Fix commits getting cut off
    Still, the size of commits need to be reduced a little bit

--- src/ui/RepoViewerWidget.cc
+++ src/ui/RepoViewerWidget.cc
@@ -3,6 +3,7 @@
 #include "../git_wrapper/Commit.h"
 #include "./ui_RepoViewerWidget.h"
 #include "Commit.h"
+#include "qlayout.h"
 
 RepoViewerWidget::RepoViewerWidget(QWidget* parent)
     : QWidget(parent), ui(new Ui::RepoViewerWidget) {
@@ -11,6 +12,13 @@ RepoViewerWidget::RepoViewerWidget(QWidget* parent)
 	git::Commit commit;
 	commit.Setup("Add generic comment", "Saurabh Sangpal", "asch23kcks", "18 Sep 2021 18:30");
 
-	auto* uiCommit = new Commit(ui->commitsScrollArea);
-	uiCommit->Setup(commit);
+	auto* layout = new QVBoxLayout;
+	ui->commitsScrollArea->setLayout(layout);
+
+	for (int i = 0; i < 10; i++) {
+		auto* uiCommit = new Commit(ui->commitsScrollArea);
+		uiCommit->Setup(commit);
+		layout->addWidget(uiCommit);
+	}
+	ui->scrollArea->setWidget(ui->commitsScrollArea);
 }
