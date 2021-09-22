0dd3174fae6aad9fbb4a3566a74af2d7ee51c734
Saurabh Sangpal <saurabhsangpal@gmail.com>
2021-09-22 19:18:24 +0530

    Add clone logic to clone dialog box

--- src/ui/CloneDialog.cc
+++ src/ui/CloneDialog.cc
@@ -1,7 +1,9 @@
 #include "CloneDialog.h"
 
 #include <qfiledialog.h>
+#include <qmessagebox.h>
 
+#include "../git_wrapper/Repository.h"
 #include "./ui_CloneDialog.h"
 
 CloneDialog::CloneDialog(QWidget* parent) : QDialog(parent), ui(new Ui::CloneDialog) {
@@ -9,6 +11,8 @@ CloneDialog::CloneDialog(QWidget* parent) : QDialog(parent), ui(new Ui::CloneDia
 
 	connect(ui->btnBrowseDestination, &QPushButton::clicked, this,
 		&CloneDialog::BrowseForDestinationFolder);
+	connect(ui->btnClone, &QPushButton::clicked, this, &CloneDialog::CloneRepo);
+	connect(ui->btnCancel, &QPushButton::clicked, this, &CloneDialog::reject);
 }
 
 void CloneDialog::BrowseForDestinationFolder() noexcept {
@@ -21,4 +25,28 @@ void CloneDialog::BrowseForDestinationFolder() noexcept {
 	}
 
 	ui->leDestination->setText(dir);
+}
+
+void CloneDialog::CloneRepo() noexcept {
+	auto url  = ui->leRepoUrl->text();
+	auto dest = ui->leDestination->text();
+
+	if (url.toStdString().empty() || dest.toStdString().empty()) {
+		const char* errUrl =
+		    "Failed to clone the repository because the provided URL is empty!";
+		const char* errDest =
+		    "Failed to clone the repository because the provided destination is empty!";
+		auto errorText = url.toStdString().empty() ? errUrl : errDest;
+		QMessageBox::warning(this, "Failed to clone", errorText);
+		return;
+	}
+
+	git::Repository repo;
+	if (!repo.Clone(url.toStdString().c_str(), dest.toStdString().c_str())) {
+		QMessageBox::warning(this, "Failed to clone", "Failed to clone the repository!");
+	}
+	else {
+		// TODO: Open the repo in UI.
+		accept();
+	}
 }
--- src/ui/CloneDialog.h
+++ src/ui/CloneDialog.h
@@ -14,6 +14,7 @@ class CloneDialog : public QDialog {
 
     private:
 	void BrowseForDestinationFolder() noexcept;
+	void CloneRepo() noexcept;
 
 	Ui::CloneDialog* ui;
 };
