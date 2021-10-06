ecda8e4a25805385abdd0ed9384c3d4ea6032be5
Saurabh Sangpal <saurabhsangpal@gmail.com>
2021-10-06 22:14:46 +0530

    Add a button to the commit UI
    The button's size is still not setup properly

--- src/ui/Commit.cc
+++ src/ui/Commit.cc
@@ -1,9 +1,16 @@
 #include "Commit.h"
 
+#include <QPushButton>
+
 #include "../git_wrapper/Commit.h"
 #include "./ui_Commit.h"
 
-Commit::Commit(QWidget* parent) : QWidget(parent), ui(new Ui::Commit) { ui->setupUi(this); }
+Commit::Commit(QWidget* parent) : QWidget(parent), ui(new Ui::Commit) {
+	ui->setupUi(this);
+	auto* btn = new QPushButton(this);
+	btn->setFlat(true);
+	btn->resize(this->size());
+}
 
 void Commit::Setup(const git::Commit& commit) noexcept {
 	ui->msg->setText(commit.GetMessage().c_str());
