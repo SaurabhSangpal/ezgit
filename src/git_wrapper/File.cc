663da36303e718a17eb98528435376e5c1ff6edb
Saurabh Sangpal <saurabhsangpal@gmail.com>
2022-02-16 19:19:05 +0530

    Add enum to represent file state

--- src/git_wrapper/File.cc
+++ src/git_wrapper/File.cc
@@ -8,3 +8,30 @@ bool git::File::IsStaged() const {
 	       status == GIT_STATUS_INDEX_DELETED || status == GIT_STATUS_INDEX_RENAMED ||
 	       status == GIT_STATUS_INDEX_TYPECHANGE;
 }
+
+git::FileState git::File::GetFileState() const {
+	switch (status) {
+		case GIT_STATUS_INDEX_NEW:
+		case GIT_STATUS_WT_NEW:
+			return git::FileState::New;
+			break;
+		case GIT_STATUS_INDEX_MODIFIED:
+		case GIT_STATUS_WT_MODIFIED:
+			return git::FileState::Modified;
+			break;
+		case GIT_STATUS_INDEX_DELETED:
+		case GIT_STATUS_WT_DELETED:
+			return git::FileState::Deleted;
+			break;
+		case GIT_STATUS_INDEX_RENAMED:
+		case GIT_STATUS_WT_RENAMED:
+			return git::FileState::Renamed;
+			break;
+		case GIT_STATUS_INDEX_TYPECHANGE:
+		case GIT_STATUS_WT_TYPECHANGE:
+			return git::FileState::Typechange;
+			break;
+		default:
+			return git::FileState::None;
+	}
+}
--- src/git_wrapper/File.h
+++ src/git_wrapper/File.h
@@ -4,6 +4,8 @@
 #include <string>
 
 namespace git {
+enum class FileState { None, New, Modified, Deleted, Renamed, Typechange };
+
 class File {
     public:
 	File(const std::string& filePath, git_status_t fileStatus);
@@ -11,6 +13,8 @@ class File {
 	const std::string& GetPath() { return path; }
 
 	bool IsStaged() const;
+
+	FileState GetFileState() const;
 
     private:
 	std::string  path;
